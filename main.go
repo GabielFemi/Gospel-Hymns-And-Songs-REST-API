0dd0cf8bb2d799916887a59326fa2588f849a617
GabielFemi <gabrielfemi799@gmail.com>
2020-05-24 23:34:49 +0200

    set status code and also encode a json string to response writer
    Took 9 minutes

--- main.go
+++ main.go
@@ -1,1 +1,22 @@
-package ghs_api
+package main
+
+import (
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/api/ghs", Index)
+	log.Println("Listening on 127.0.0.1:7000")
+	_ = http.ListenAndServe(":7000", router)
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	_ = json.NewEncoder(w).Encode("This is the GHS")
+
+}
