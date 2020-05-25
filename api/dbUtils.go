132072436ab419fdd6a746a9a76288d3bbc68e5f
GabielFemi <gabrielfemi799@gmail.com>
2020-05-25 19:10:14 +0200

    parse ghs into struct

--- api/dbUtils.go
+++ api/dbUtils.go
@@ -27,12 +27,23 @@ func GetAllGhsFromDb() {
 	}
 	var id int
 	var title, chorus, stanza1, stanza2, stanza3, stanza4, stanza5, stanza6, stanza7, stanza8 string
+	var ghs GHS
 	for fetchAllGhsFromDB.Next() {
 		err := fetchAllGhsFromDB.Scan(&id, &title, &chorus, &stanza1, &stanza2, &stanza3, &stanza4, &stanza5, &stanza6, &stanza7, &stanza8)
 		if err != nil {
 			panic(err)
 		}
-
+		ghs.Id = id
+		ghs.Title = title
+		ghs.Chorus = chorus
+		ghs.Stanza1 = stanza1
+		ghs.Stanza2 = stanza2
+		ghs.Stanza3 = stanza3
+		ghs.Stanza4 = stanza4
+		ghs.Stanza5 = stanza5
+		ghs.Stanza6 = stanza6
+		ghs.Stanza8 = stanza8
 	}
+
 
 }
