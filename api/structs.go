57a7aed5af2a9dd208e2f3be2a02cc0475c2637f
GabielFemi <gabrielfemi799@gmail.com>
2020-05-25 19:00:10 +0200

    add structs file to contain the various datatypes for the program

---
+++ api/structs.go
@@ -0,0 +1,15 @@
+package ghs_api
+
+type GHS struct {
+	Id       int
+	Title    string
+	Chorus   string
+	Stanza_1 string
+	Stanza_2 string
+	Stanza_3 string
+	Stanza_4 string
+	Stanza_5 string
+	Stanza_6 string
+	Stanza_7 string
+	Stanza_8 string
+}
